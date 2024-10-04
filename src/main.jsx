4138d29488b55636043e0ce05df7d1162c7ae147
NichithaAllakonda <anichitha2004@gamil.com>
2024-10-04 17:48:39 +0530

    Timer

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Timer from './Components/Timer.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Timer />
+  </StrictMode>,
+)
